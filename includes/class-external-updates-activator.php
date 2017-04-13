8653718fe843778e8a97adfcc5241f2e3209a3ab
stiofan <info@wpgeodirectory.com>
2017-04-13 17:48:20 +0100

    copy over GD licences fixed

--- includes/class-external-updates-activator.php
+++ includes/class-external-updates-activator.php
@@ -44,94 +44,94 @@ class External_Updates_Activator {
 
 					foreach($plugins as $key => $plugin){
 
-						if($key=='geodir_advance_search_filters/geodir_advance_search_filters.php' && isset($current_keys['geodiradvancesearch'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodiradvancesearch']['status'],
-								'key'       =>  $current_keys['geodiradvancesearch']['license'],
+						if($key=='geodir_advance_search_filters/geodir_advance_search_filters.php' && isset($licence_keys['geodiradvancesearch'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodiradvancesearch']['status'],
+								'key'       =>  $licence_keys['geodiradvancesearch']['licence'],
 							);
-						}elseif($key=='geodir_affiliate/geodir_affiliate.php' && isset($current_keys['gdaffiliate'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['gdaffiliate']['status'],
-								'key'       =>  $current_keys['gdaffiliate']['license'],
+						}elseif($key=='geodir_affiliate/geodir_affiliate.php' && isset($licence_keys['gdaffiliate'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['gdaffiliate']['status'],
+								'key'       =>  $licence_keys['gdaffiliate']['licence'],
 							);
-						}elseif($key=='geodir_ajax_duplicate_alert/geodir_ajax_duplicate_alert.php' && isset($current_keys['geodir_duplicatealert'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir_duplicatealert']['status'],
-								'key'       =>  $current_keys['geodir_duplicatealert']['license'],
+						}elseif($key=='geodir_ajax_duplicate_alert/geodir_ajax_duplicate_alert.php' && isset($licence_keys['geodir_duplicatealert'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir_duplicatealert']['status'],
+								'key'       =>  $licence_keys['geodir_duplicatealert']['licence'],
 							);
-						}elseif($key=='geodir_buddypress/geodir_buddypress.php' && isset($current_keys['gdbuddypress'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['gdbuddypress']['status'],
-								'key'       =>  $current_keys['gdbuddypress']['license'],
+						}elseif($key=='geodir_buddypress/geodir_buddypress.php' && isset($licence_keys['gdbuddypress'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['gdbuddypress']['status'],
+								'key'       =>  $licence_keys['gdbuddypress']['licence'],
 							);
-						}elseif($key=='geodir_claim_listing/geodir_claim_listing.php' && isset($current_keys['geodirclaim'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodirclaim']['status'],
-								'key'       =>  $current_keys['geodirclaim']['license'],
+						}elseif($key=='geodir_claim_listing/geodir_claim_listing.php' && isset($licence_keys['geodirclaim'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodirclaim']['status'],
+								'key'       =>  $licence_keys['geodirclaim']['licence'],
 							);
-						}elseif($key=='geodir_custom_google_maps/geodir_custom_google_maps.php' && isset($current_keys['geodir_customgmaps'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir_customgmaps']['status'],
-								'key'       =>  $current_keys['geodir_customgmaps']['license'],
+						}elseif($key=='geodir_custom_google_maps/geodir_custom_google_maps.php' && isset($licence_keys['geodir_customgmaps'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir_customgmaps']['status'],
+								'key'       =>  $licence_keys['geodir_customgmaps']['licence'],
 							);
-						}elseif($key=='geodir_custom_posts/geodir_custom_posts.php' && isset($current_keys['geodir_custom_posts'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir_custom_posts']['status'],
-								'key'       =>  $current_keys['geodir_custom_posts']['license'],
+						}elseif($key=='geodir_custom_posts/geodir_custom_posts.php' && isset($licence_keys['geodir_custom_posts'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir_custom_posts']['status'],
+								'key'       =>  $licence_keys['geodir_custom_posts']['licence'],
 							);
-						}elseif($key=='geodir_event_manager/geodir_event_manager.php' && isset($current_keys['geodirevents'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodirevents']['status'],
-								'key'       =>  $current_keys['geodirevents']['license'],
+						}elseif($key=='geodir_event_manager/geodir_event_manager.php' && isset($licence_keys['geodirevents'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodirevents']['status'],
+								'key'       =>  $licence_keys['geodirevents']['licence'],
 							);
-						}elseif($key=='geodir_franchise/geodir_franchise.php' && isset($current_keys['geodir-franchise'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir-franchise']['status'],
-								'key'       =>  $current_keys['geodir-franchise']['license'],
+						}elseif($key=='geodir_franchise/geodir_franchise.php' && isset($licence_keys['geodir-franchise'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir-franchise']['status'],
+								'key'       =>  $licence_keys['geodir-franchise']['licence'],
 							);
-						}elseif($key=='geodir_list_manager/geodir_list_manager.php' && isset($current_keys['geodirlists'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodirlists']['status'],
-								'key'       =>  $current_keys['geodirlists']['license'],
+						}elseif($key=='geodir_list_manager/geodir_list_manager.php' && isset($licence_keys['geodirlists'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodirlists']['status'],
+								'key'       =>  $licence_keys['geodirlists']['licence'],
 							);
-						}elseif($key=='geodir_location_manager/geodir_location_manager.php' && isset($current_keys['geodirlocation'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodirlocation']['status'],
-								'key'       =>  $current_keys['geodirlocation']['license'],
+						}elseif($key=='geodir_location_manager/geodir_location_manager.php' && isset($licence_keys['geodirlocation'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodirlocation']['status'],
+								'key'       =>  $licence_keys['geodirlocation']['licence'],
 							);
-						}elseif($key=='geodir_marker_cluster/geodir_marker_cluster.php' && isset($current_keys['geodir_markercluster'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir_markercluster']['status'],
-								'key'       =>  $current_keys['geodir_markercluster']['license'],
+						}elseif($key=='geodir_marker_cluster/geodir_marker_cluster.php' && isset($licence_keys['geodir_markercluster'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir_markercluster']['status'],
+								'key'       =>  $licence_keys['geodir_markercluster']['licence'],
 							);
-						}elseif($key=='geodir_payment_manager/geodir_payment_manager.php' && isset($current_keys['geodir_payments'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir_payments']['status'],
-								'key'       =>  $current_keys['geodir_payments']['license'],
+						}elseif($key=='geodir_payment_manager/geodir_payment_manager.php' && isset($licence_keys['geodir_payments'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir_payments']['status'],
+								'key'       =>  $licence_keys['geodir_payments']['llicence'],
 							);
-						}elseif($key=='geodir_recaptcha/geodir_recaptcha.php' && isset($current_keys['geodir-recaptcha'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir-recaptcha']['status'],
-								'key'       =>  $current_keys['geodir-recaptcha']['license'],
+						}elseif($key=='geodir_recaptcha/geodir_recaptcha.php' && isset($licence_keys['geodir-recaptcha'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir-recaptcha']['status'],
+								'key'       =>  $licence_keys['geodir-recaptcha']['licence'],
 							);
-						}elseif($key=='geodir_review_rating_manager/geodir_review_rating_manager.php' && isset($current_keys['geodir_reviewratings'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir_reviewratings']['status'],
-								'key'       =>  $current_keys['geodir_reviewratings']['license'],
+						}elseif($key=='geodir_review_rating_manager/geodir_review_rating_manager.php' && isset($licence_keys['geodir_reviewratings'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir_reviewratings']['status'],
+								'key'       =>  $licence_keys['geodir_reviewratings']['licence'],
 							);
-						}elseif($key=='geodir_social_importer/geodir_social_importer.php' && isset($current_keys['geodir_socialimporter'])){
-							$licence_keys[$key] = (object) array(
-								'license'   =>  $current_keys['geodir_socialimporter']['status'],
-								'key'       =>  $current_keys['geodir_socialimporter']['license'],
+						}elseif($key=='geodir_social_importer/geodir_social_importer.php' && isset($licence_keys['geodir_socialimporter'])){
+							$current_keys[$key] = (object) array(
+								'license'   =>  $licence_keys['geodir_socialimporter']['status'],
+								'key'       =>  $licence_keys['geodir_socialimporter']['licence'],
 							);
 						}
 
 					}
 
-					if(!empty($licence_keys)){
-						update_site_option( 'exup_keys', $licence_keys  ); // update network option
+					if(!empty($current_keys)){
+						update_site_option( 'exup_keys', $current_keys  ); // update network option
 
-						update_option( 'exup_keys', $licence_keys ); // update single site option
+						update_option( 'exup_keys', $current_keys ); // update single site option
 					}
 
 				}
