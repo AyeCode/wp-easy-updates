66489da9b350f17df7ed7e2e467f930c40c550a4
stiofan <info@wpgeodirectory.com>
2017-06-28 19:12:06 +0100

    1.1.2

--- external-updates.php
+++ external-updates.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WP Easy Updates
  * Plugin URI:        http://ayecode.io/
  * Description:       Update plugins provided by EDD software licencing or via github with ease.
- * Version:           1.1.1
+ * Version:           1.1.2
  * Author:            AyeCode Ltd
  * Author URI:        https://ayecode.io/
  * License:           GPL-2.0+
@@ -32,7 +32,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 // Define the version number
-define('WP_EASY_UPDATES_VERSION', '1.1.1');
+define('WP_EASY_UPDATES_VERSION', '1.1.2');
 
 // Define a constant that can be checked against for easy checking of activation status.
 define('WP_EASY_UPDATES_ACTIVE', true);
